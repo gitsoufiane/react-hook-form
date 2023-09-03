ce5b0e7b40625571eee64108471fe1bfc6b51253
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-02 23:00:27 -0700

    default data

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -14,7 +14,25 @@ export function YoutubeForm() {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>();
+  } = useForm<FormValues>({
+    defaultValues: {
+      username: "soufiane",
+      email: "soufiane@email.com",
+      channel: "10",
+    },
+    //! for Async Default data
+    // defaultValues: async () => {
+    //   const response = await fetch(
+    //     "https://jsonplaceholder.typicode.com/users/1"
+    //   );
+    //   const data = await response.json();
+    //   return {
+    //     username: data.username,
+    //     email: data.email,
+    //     channel: "10",
+    //   };
+    // },
+  });
   renderCount++;
 
   const onSubmit = (data: FormValues) => {
