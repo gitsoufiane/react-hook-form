c97bc2e0b4bf2f7f2f230738adcc550c93e5d155
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-03 11:44:26 -0700

    reset form

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -28,6 +28,7 @@ export function YoutubeForm() {
     watch,
     getValues,
     setValue,
+    reset,
     formState: {
       errors,
       touchedFields,
@@ -290,6 +291,12 @@ export function YoutubeForm() {
           </div>
         </div>
         <div className="flex flex-row gap-10">
+          <button
+            className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
             onClick={() => console.log(getValues())}
