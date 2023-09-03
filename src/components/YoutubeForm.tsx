843f1190b14ceb2642e698f476853bde3e20dbeb
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-03 10:08:59 -0700

    disabled field

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -175,7 +175,7 @@ export function YoutubeForm() {
             htmlFor="birthDate"
             className="block text-gray-700 text-sm font-bold mb-2"
           >
-            Age
+            Date of birth
           </label>
           <input
             type="date"
@@ -197,7 +197,9 @@ export function YoutubeForm() {
             type="text"
             id="twitter"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            {...register("social.twitter")}
+            {...register("social.twitter", {
+              disabled: watch("channel") === "",
+            })}
           />
         </div>
         <div>
