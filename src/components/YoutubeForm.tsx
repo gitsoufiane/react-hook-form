15d3e27c7d1c3d62abfed4320a30b3cbf4633318
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-02 22:25:51 -0700

    forma validation

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -20,12 +20,31 @@ export function YoutubeForm() {
     <div>
       <h1>YouTube Form --- render ({renderCount})</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "username is required",
+            },
+          })}
+        />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value:
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "Invalid email",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel")} />
