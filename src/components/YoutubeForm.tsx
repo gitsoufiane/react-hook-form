9196eb9e1567d1dc23a7024a57b5a6f9c71f4908
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-09-03 12:17:17 -0700

    manually trigger validation

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -30,6 +30,7 @@ export function YoutubeForm() {
     getValues,
     setValue,
     reset,
+    trigger,
     formState: {
       errors,
       touchedFields,
@@ -305,6 +306,12 @@ export function YoutubeForm() {
           </div>
         </div>
         <div className="flex flex-row gap-10">
+          <button
+            className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+            onClick={() => trigger()}
+          >
+            Validate
+          </button>
           <button
             className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
             onClick={() => reset()}
